2d75c369ed64a0a4d886d85d9c7d6d55ab82b149
Gregory Ross <gregross.dev@gmail.com>
2022-12-16 11:17:06 -0500

    config site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jimmy-smith.netlify.app', 
   integrations: [tailwind()]
 });
