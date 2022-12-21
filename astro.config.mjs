56115efe84a000f466abedf2276acbf79c4870fa
Gregory Ross <gregross.dev@gmail.com>
2022-12-21 15:20:32 -0500

    add netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,33 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jimmy-smith.netlify.app', 
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+     NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          {
+            label: 'December',
+            name: 'december',
+            folder: 'src/pages/date/12',
+            create: true,
+            delete: true,
+            fields: [
+              { label: 'Date',  name: 'date', widget: 'datetime',  date_format: "MM.DD", time_format: false },
+              { label: 'Title', name: 'title', widget: 'string'  },
+              { label: 'Body' , name: 'body', widget: 'markdown'},
+            ],
+          },
+        ],
+      },
+    }),
+  ]
 });
