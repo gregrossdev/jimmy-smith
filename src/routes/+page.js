177367ba6898f3c4950540fc5e7b916f5bc0a004
Gregory Ross <77366132+gregrossdev@users.noreply.github.com>
2024-02-29 07:55:56 -0500

    Update +page.js

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -5,8 +5,9 @@ export const load = async ({fetch}) => {
 
 
     const fetchDates = async () => {
-        const response = await fetch(`https://jimmy-smith.gregross.host/api/dates/2023-${today}`)
-        const api = await response.json()
+        const response2023 = await fetch(`https://jimmy-smith.gregross.host/api/dates/2023-${today}`)
+        const response2024 = await fetch(`https://jimmy-smith.gregross.host/api/dates/2024-${today}`)
+        const api = await response2023.json() || response2024.json();
         const data = api.data;
         return data;
     }
