516d423bb8aea29406a9a17fc884d76faa6eea60
gregrosshost <159476797+gregrosshost@users.noreply.github.com>
2024-03-15 13:40:01 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,14 +13,8 @@
 
 
 <article class="prose">
-   <!-- <h2>{date.slice(5)}</h2> 
+    <h2>{date.slice(5)}</h2> 
     <h3>{title}</h3>
     <section>{@html content}</section>
--->
-<h2>Forgiveness and Balance</h2>
-<section> 
-<p><strong><em>02-29 [Leap Year]</em></strong></p><p>Forgiveness was something I had heard about a lot in church growing up, but it was something that I really didn’t understand. It was clear to me that this was a spiritual concept about which I needed to learn more. At any rate the next day after this meeting, I made a phone call to schedule a meeting; and indeed in the following weeks I did learn much about this spiritual concept. I also learned why it was not only important but necessary to keep the God of my understanding in the center of my life. It was about balance. I had gotten so out of balance spiritually because of my addiction. Now I had to learn about balance.</p><p>In our <strong><em>Basic Text, Chapter Four, How It Works, Step Eight</em></strong> it tells us: <em>“The Eighth Step starts the process of forgiveness: We forgive others; possibly we are forgiven; and finally we forgive ourselves and learn how to live in the world. By the time we reach this step, we have become ready to understand rather than to be understood. We can live and let live easier when we know the areas in which we owe amends.”</em></p><p>In our <strong><em>Living Clean: The Journey Continues, Chapter 3: A Spiritual Path, A Spiritual Journey</em></strong> it also tells us: <em>“As our spirituality develops, we become increasingly grateful for our lives and the people in them. The more good we can see in the world around us, the more grateful we become for the power that brings us to that awareness. When our bodies, minds, and spirits are in harmony, our lives show the difference. We are able to live in balance.”</em></p><p><em>“Spirituality is the progressive replacement of love in my heart where before there was only fear. This is happening because I have a connection with a loving God and people in the program.\"</em> <strong><em>Dora S., Brazil, NA Way Magazine, JULY 1999, VOLUME SIXTEEN, NUMBER THREE</em></strong></p><p><em>(Amends, Awareness, Balance, Forgiveness, God of my understanding, Grateful, Harmony, Learn, Love in my Heart, Ready To Understand, Spiritual Concept, Spiritually, Steps)</em></p>
-
-</section>
 </article>
 
